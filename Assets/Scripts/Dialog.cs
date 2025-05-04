64a13c7f8705ad0c61d2d62d57a57ac24ee6e1de
hbks0224 <hbks1234@gmail.com>
2025-05-04 19:37:52 +0900

    Update Dialog.cs
    대화수정

--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -15,8 +15,7 @@ public class Dialog : MonoBehaviour
     {
         //일단 꺼둠
         Check_F.SetActive(false); //F 팝업
-        //DialogUI.SetActive(false); //대화 팝업
-        //Check_Y_or_N.SetActive(false); //대화 팝업
+
         foreach (GameObject go in Check_List) 
         { 
             go.SetActive(false);
@@ -25,25 +24,28 @@ public class Dialog : MonoBehaviour
 
     }
 
-    private void OnTriggerStay2D(Collider2D other)             //단 한번만 호출
+    private void OnTriggerStay2D(Collider2D other)          
     {
-        if (other.CompareTag("Player") )
+        if (other.CompareTag("Player") ) //태그가 플레이어면
         {
             Debug.Log("플레이어 들어옴");
-            Check_F.SetActive(true); //플레이어와의 트리거 작동시 ui 켜주기
-
+            //플레이어와의 트리거 작동시 ui 켜주기
+           // Check_F.SetActive(true);
             if (Input.GetKeyDown(KeyCode.F))
             {
+                
                 Debug.Log("F Input Check");
                 //DialogUI.SetActive(true);
                 foreach (GameObject go in Check_List)
                 {
                     go.SetActive(true);
-
-
-                    
-
                 }
+                if (Check_F.activeSelf)
+                {
+                    Check_F.SetActive(false);
+                     
+                }
+    
                 //if (Check_F.activeSelf && Check_List[0].activeSelf && Check_List[1].activeSelf)
                 //{
 
@@ -73,6 +75,15 @@ public class Dialog : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D collision) //이렇게 check_F를 분리해서OnTriggerEnter로 안하면 지속적으로 계속 켜놔서 F를 눌러도 사라지자마자 다시 생김
+    {
+        if ( collision.CompareTag("Player"))
+        {
+            Check_F.SetActive(true);
+
+        }
+    }
+
 
     //private void YesOrNo()
     //{
@@ -80,7 +91,7 @@ public class Dialog : MonoBehaviour
     //    if()
 
     //}
-    
+
 
 
 }
